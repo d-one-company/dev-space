42b5c72c4fbac56a5d64967d63f113509b094316
banidja <branislav.jaksic@d-one.design>
2024-05-20 16:57:40 +0200

    feat: add editor

--- components/Composer/Composer.tsx
+++ components/Composer/Composer.tsx
@@ -1,44 +1,54 @@
 'use client';
 
-import { CodeNode } from '@lexical/code';
-import { LinkNode } from '@lexical/link';
+import { CodeHighlightNode, CodeNode } from '@lexical/code';
+import { AutoLinkNode, LinkNode } from '@lexical/link';
 import { ListItemNode, ListNode } from '@lexical/list';
 import { TRANSFORMERS } from '@lexical/markdown';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
 import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import exampleTheme from './ExampleTheme';
+import ToolbarPlugin from './Toolbar';
+import './composer.css';
+import CodeHighlightPlugin from './plugins/CodeHighlightPlugin';
 
-const theme = {
-  // Theme styling goes here
+const editorConfig = {
+  // The editor theme
+  namespace: 'rich-text-editor',
+  theme: exampleTheme,
+  onError(error: Error) {
+    throw error;
+  },
+  // Any custom nodes go here
+  nodes: [HeadingNode, ListNode, ListItemNode, QuoteNode, CodeNode, CodeHighlightNode, AutoLinkNode, LinkNode],
 };
 
-function onError(error: Error) {
-  console.error(error);
-}
-
-const Editor = () => {
-  const initialConfig = {
-    namespace: 'MyEditor',
-    theme,
-    onError,
-    nodes: [HeadingNode, QuoteNode, CodeNode, ListNode, ListItemNode, LinkNode],
-  };
-
+export default function Editor() {
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={<ContentEditable className="border-gold-drop flex h-80 w-full rounded-xl border p-2 outline-none" />}
-        placeholder={<div className="absolute left-2 top-2">Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <HistoryPlugin />
-      <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="">
+        <ToolbarPlugin />
+        <div className="">
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="border-gold-drop flex min-h-80 w-full flex-col rounded-xl border p-2 outline-none" />}
+            placeholder={<div className="absolute left-[9px] top-[54px]">Enter some text...</div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <CodeHighlightPlugin />
+          <ListPlugin />
+          <LinkPlugin />
+          <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+        </div>
+      </div>
     </LexicalComposer>
   );
-};
-
-export default Editor;
+}
