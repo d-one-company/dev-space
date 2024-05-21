d1f4f5a84ecba76e1bc841eca48f89b7a8e8c52c
banidja <branislav.jaksic@d-one.design>
2024-05-21 12:39:42 +0200

    feat: update sidebar

--- components/icons/Message.tsx
+++ components/icons/Message.tsx
@@ -1,9 +1,9 @@
-const Bookmark = () => {
+const Message = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-      <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
+      <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
     </svg>
   );
 };
 
-export default Bookmark;
+export default Message;
