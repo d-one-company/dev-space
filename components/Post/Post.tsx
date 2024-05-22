b1cecff5935eeb5082cfc79bbda9200113c2704d
vukasin <vukasin.mladenovic@d-one.design>
2024-05-22 13:59:19 +0200

    fix: post info

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -2,9 +2,8 @@ import { Bookmark } from 'lucide-react';
 import { Button } from '../Button';
 import PostActions from './PostActions/PostActions';
 import PostContent from './PostContent';
-import { getTimePassed } from '@/lib/utils/getTimePassed';
 import type { Post as TPost } from '@/types/posts';
-import { Avatar, AvatarImage } from '../Avatar';
+import PostInfo from './PostInfo';
 
 type PostProps = {
   post: TPost;
@@ -14,15 +13,7 @@ const Post = ({ post }: PostProps) => {
   return (
     <div className="border-davy-gray flex w-full flex-col gap-4 rounded-lg border p-4">
       <div className="flex w-full items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Avatar className="size-9">
-            <AvatarImage src={post.author?.image || 'https://github.com/shadcn.png'} />
-          </Avatar>
-          <div className="flex flex-col items-start justify-between">
-            <p className="text-base">{post.author?.name}</p>
-            <p className="text-davy-gray text-sm">{getTimePassed(post.createdAt)}</p>
-          </div>
-        </div>
+        <PostInfo author={post.author} createdAt={post.createdAt} />
         <Button className="text-davy-gray bg-transparent transition-colors duration-200 hover:bg-transparent hover:text-white">
           <Bookmark fill="currentColor" />
         </Button>
--- components/Post/PostInfo.tsx
+++ components/Post/PostInfo.tsx
@@ -1,11 +1,13 @@
+import { getTimePassed } from '@/lib/utils/getTimePassed';
 import { Avatar, AvatarImage } from '../Avatar';
 import type { Post } from '@/types/posts';
 
-type PostProfileProps = {
+type PostInfo = {
   author: Post['author'];
+  createdAt: Date;
 };
 
-const PostProfile = ({ author }: PostProfileProps) => {
+const PostInfo = ({ author, createdAt }: PostInfo) => {
   return (
     <div className="flex items-center gap-2">
       <Avatar className="size-9">
@@ -13,10 +15,10 @@ const PostProfile = ({ author }: PostProfileProps) => {
       </Avatar>
       <div className="flex flex-col items-start justify-between">
         <p className="text-base">{author?.name}</p>
-        <p className="text-davy-gray text-sm">2 hours ago</p>
+        <p className="text-davy-gray text-sm">{getTimePassed(createdAt)}</p>
       </div>
     </div>
   );
 };
 
-export default PostProfile;
+export default PostInfo;
