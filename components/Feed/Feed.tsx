49eb5feb23def0d92af23ae08ffb80aa210cd765
Branislav Jaksic <branislav.jaksic@d-one.design>
2024-05-22 13:18:42 +0200

    Update components/Feed/Feed.tsx
    Co-authored-by: blink-dev <nikola.mirjanic@d-one.design>

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -9,7 +9,7 @@ const Feed = () => {
       <div className="relative w-full">
         <Editor />
       </div>
-      <Tabs defaultValue="account" className="mt-5 w-full ">
+      <Tabs defaultValue="account" className="mt-5 w-full">
         <TabsList className="gap-5">
           <TabsTrigger className="bg-gondola flex items-center gap-2 rounded-lg px-4 py-2" value="account">
             <Heart size={18} fill="#F43F41" stroke="#F43F41" />
