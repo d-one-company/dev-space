5ac7e3d646236bb0e046da48938823fdf99d697e
vukasin <vukasin.mladenovic@d-one.design>
2024-05-22 14:56:03 +0200

    feat: add reserved usernames

--- lib/utils/generateUniqueUsername.ts
+++ lib/utils/generateUniqueUsername.ts
@@ -2,6 +2,7 @@ import { db } from '@/db';
 import { eq } from 'drizzle-orm';
 import { users } from '@/db/schema';
 import { nanoid } from 'nanoid';
+import { RESERVED_USERNAMES } from '../../next.constants.mjs';
 
 export default async function generateUniqueUsername(email: string, suffix?: string): Promise<string> {
   const emailBase = email.replace(/@.+/, '').replace(/[&/\\#,+()$~%._@'":*?<>{}]/g, '');
@@ -9,7 +10,7 @@ export default async function generateUniqueUsername(email: string, suffix?: str
 
   const existingUser = await db.query.users.findFirst({ where: eq(users.username, username) });
 
-  if (existingUser) {
+  if (existingUser || RESERVED_USERNAMES.includes(username)) {
     return await generateUniqueUsername(email, nanoid(5));
   }
 
