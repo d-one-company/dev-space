4244175e5d4aca0722b982f626532ad41485162b
vukasin <vukasin.mladenovic@d-one.design>
2024-05-23 16:47:41 +0200

    fix: fix migration files

--- db/drizzle/0004_vengeful_gateway.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "dev_space_feed" ADD COLUMN "postAuthorId" text;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "dev_space_feed" ADD CONSTRAINT "dev_space_feed_postAuthorId_dev_space_user_id_fk" FOREIGN KEY ("postAuthorId") REFERENCES "public"."dev_space_user"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
