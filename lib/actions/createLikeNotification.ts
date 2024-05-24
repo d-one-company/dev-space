dac0b70be53ab9afa390f725b493e7a9af1be1b7
vukasin <vukasin.mladenovic@d-one.design>
2024-05-24 12:29:43 +0200

    feat: like notifications

--- lib/actions/createLikeNotification.ts
+++ lib/actions/createLikeNotification.ts
@@ -1,16 +1,28 @@
-import { notifications, users } from '@/db/schema';
+import { notifications, posts, users } from '@/db/schema';
 import pusherServer from '../pusher/pusherServer';
 import { db } from '@/db';
 import { eq } from 'drizzle-orm';
 import { Notification } from '@/types/notifications';
 
-export default async function createLikeNotification(postId: string, creatorId: string, receiverId: string) {
+export default async function createLikeNotification(postId: string, creatorId: string) {
+  const post = await db.query.posts.findFirst({
+    where: eq(posts.id, postId),
+    columns: {
+      userId: true,
+    },
+  });
+
+  if (!post) {
+    console.error('Failed to create notification: post not found');
+    return;
+  }
+
   const [notificationData] = await db
     .insert(notifications)
     .values({
       postId,
       createdBy: creatorId,
-      userId: receiverId,
+      userId: post.userId,
       type: 'like',
     })
     .returning({ id: notifications.id, type: notifications.type, createdAt: notifications.createdAt, postId: notifications.postId, isRead: notifications.isRead });
@@ -24,7 +36,7 @@ export default async function createLikeNotification(postId: string, creatorId: 
     creator: creatorData,
   };
 
-  await pusherServer.trigger(receiverId, 'notification:new', notification);
+  await pusherServer.trigger(post.userId, 'notification:new', notification);
 
   return notification as Notification;
 }
