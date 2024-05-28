17992011429ab987229d48039d3bc8741d93f6a3
banidja <branislav.jaksic@d-one.design>
2024-05-28 16:47:10 +0200

    fix: modal ui

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -15,17 +15,9 @@ const DialogPortal = DialogPrimitive.Portal;
 const DialogClose = DialogPrimitive.Close;
 
 const DialogOverlay = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Overlay>, React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>>(
-  ({ className, ...props }, ref) => (
-    <DialogPrimitive.Overlay
-      ref={ref}
-      className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-[2000] bg-black',
-        className
-      )}
-      {...props}
-    />
-  )
+  ({ className, ...props }, ref) => <DialogPrimitive.Overlay ref={ref} className={cn('fixed inset-0 z-[2000] flex', className)} {...props} />
 );
+
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Content>, React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>>(
@@ -42,7 +34,7 @@ const DialogContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.C
       >
         {children}
         <DialogPrimitive.Close className="data-[state=open]:bg-accent data-[state=open]:text-muted-foreground focus:ring-ring absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-          <X className="h-4 w-4" />
+          <X className="text-primary-foreground h-4 w-4" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -8,7 +8,7 @@ export const TopBar = async () => {
 
   return (
     <div className="border-secondary-bg sticky top-0 z-[1000] flex h-[60px] w-full items-start border-b bg-night py-3 pl-10 pr-5">
-      <p className="min-w-[290px] whitespace-nowrap text-oslo-gray">dev-space</p>
+      <p className="mt-1.5 min-w-[290px] whitespace-nowrap text-oslo-gray">dev-space</p>
       <Search users={users as unknown as UserProfile[]} />
       <div className="flex min-w-[475px] items-end justify-end">
         <ThemeSwitch />
